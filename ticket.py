2f013cbcd22c87dd4c753c4a1be82aa04db4f23d
Tazzle <tazzle@tazzle.com>
2017-04-02 21:22:41 +0100

    PUT method for updating existing tickets

--- ticket.py
+++ ticket.py
@@ -1,7 +1,16 @@
 import random
 import uuid
 
+def generate_lines(num_of_lines):
+    addendum = []
+    for x in range(0,num_of_lines):
+        #generate three lines with three random numbers between 1-3
+        line = [random.randrange(1,4) for x in range(0,3)]
+        addendum.append(line)
+    return addendum
+
 class Ticket():
+
 
     def __init__(self, lines):
         self.ticket_lines = []
--- ticket_service.py
+++ ticket_service.py
@@ -28,13 +28,12 @@ class TicketResource(object):
 
     
     def on_post(self, req, resp):
-        #generates ticket
-        #todo: create on_put to modify existing ticket
-        if req.get_param("lines"):
-            num_of_lines = int(req.get_param("lines"))
+        #generates new ticket ticket
+        if req.get_param_as_int("lines"):
+            num_of_lines = req.get_param("lines")
             #number of lines should be between 1 and 3
-            if(num_of_lines <= 0 or num_of_lines > 3):
-                resp.body = '{"message": "number of lines should be between 1 and 3"}'
+            if(num_of_lines <= 0):
+                resp.body = '{"message": "number of lines should be at least 1"}'
                 resp.status = falcon.HTTP_400
             else:
                 #generate new ticket
@@ -43,13 +42,38 @@ class TicketResource(object):
                 filename = str(new_ticket.id) + '.html'
                 with open(os.path.join(self.ticket_store, filename), 'wb') as temp_file:
                     for line in new_ticket.ticket_lines:
-                        temp_file.write("<body>" + str(line) + "</body>")
+                        temp_file.write("<p>" + str(line) + "</p>")
                 #return new ticket ID with response
                 resp.body = json.dumps("New ticket generated with " + str(num_of_lines) + " lines, ticket ID = " + str(new_ticket.id))
                 resp.status = falcon.HTTP_200
         else:
-            resp.body = '{"message": "number of lines not provided"}'
+            resp.body = json.dumps("Expects 1 parameter; 'lines")
             resp.status = falcon.HTTP_400
+
+    #test 
+    #when working with multiple parameters put url into quotes otherwise only recognises first argument
+    #in the shell the ampersand is used for forking processes so does not behave as expected    
+    #http PUT "localhost:8000/ticket?lines=3&id=10e477e2-1458-447c-8e83-80c37f024225"        
+    def on_put(self, req, resp):
+        if req.get_param("id") and req.get_param("lines"):
+            ticket_id = req.get_param("id")
+            num_of_lines = req.get_param("lines")
+            f = open(os.path.join(self.ticket_store, ticket_id + ".html"), 'a')
+            if(f):
+                new_lines = ticket.generate_lines(num_of_lines)
+                for line in new_lines:
+                   f.write("<p>" + str(line) + "</p>")
+                resp.body = json.dumps("Ticket " + ticket_id + " updated with " + num_of_lines + " lines")
+                resp.status = falcon.HTTP_200
+                f.close()
+            else:
+                resp.body = json.dumps("File not found")
+                resp.status = falcon.HTTP_404            
+        else:
+            resp.body = json.dumps("Expects 2 parameters; 'ID' and 'lines'")
+            resp.status = falcon.HTTP_400
+           
+
 
         
        
